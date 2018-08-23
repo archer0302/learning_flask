6f141df8605b09175618515bb5a12d48851dead2
Archer Chang <archer.chang32@gmail.com>
2018-08-23 15:18:50 +1000

    to signup

--- routes.py
+++ routes.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect, url_for
+from models import db, User
+from forms import SignupForm
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/learning_flask'
+db.init_app(app)
+
+app.secret_key = "development-key"
 
 
 @app.route("/")
@@ -12,6 +19,28 @@ def index():
 def about():
     return render_template("about.html")
 
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    form = SignupForm()
+
+    if request.method == 'POST':
+        if not form.validate():
+            return render_template('signup.html', form=form)
+        else:
+            new_user = User(form.first_name.data, form.last_name.data, form.email.data, form.password.data)
+            db.session.add(new_user)
+            db.session.commit()
+
+            session['email'] = new_user.email
+            return redirect(url_for('home'))
+
+    elif request.method == 'GET':
+        return render_template('signup.html', form=form)
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
